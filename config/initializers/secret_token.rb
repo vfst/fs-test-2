6e942dafba4dd546f24ff6949d5dcea12531828e
Vasily Polovnyov <vast@blknot.ru>
2012-10-31 11:44:49 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FsTest2::Application.config.secret_token = '56447e4bfaa3fb907c1d6c620a0539d0a50a7f99d7a1bb13e5efb3d9e9c9a301bbeac992eaa9a0841256726cccc42193cd0162444df696639a7936fa52475910'
