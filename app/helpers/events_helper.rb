3fc836a09be88df272c04d1c3e6119e325358c05
Vasily Polovnyov <vast@blknot.ru>
2012-11-06 13:35:02 +0400

    dont use parentheses with link_to helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,13 +4,13 @@ module EventsHelper
     link_opts = { class: :alternate }
     link_opts[:data] = { dismiss: :modal } if request.xhr?
 
-    link_to(t('event.actions.cancel'), events_path, link_opts)
+    link_to t('event.actions.cancel'), events_path, link_opts
   end
 
   def link_to_destroy_event(event)
     # TODO: :confirm с учетом ситуации: удаляется единичное событие или куча повторяющихся
     link_opts = { method: :delete, class: :destroy, title: t('event.actions.destroy') }
     link_opts[:remote] = request.xhr?
-    link_to('', event_path(event), link_opts)
+    link_to '', event_path(event), link_opts
   end
 end
