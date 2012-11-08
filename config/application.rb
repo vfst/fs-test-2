38b7b6bb6a42f1bec47810afae140593ad3b5276
Vasily Polovnyov <vast@blknot.ru>
2012-11-08 14:59:56 +0400

    configure correctly asset pipeline in development: config.assets.prefix

--- config/application.rb
+++ config/application.rb
@@ -56,9 +56,10 @@ module FsTest2
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['print.css', 'ie.css']
+    config.assets.precompile += %w(print.css ie.css)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile=false
   end
 end
